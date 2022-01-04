3d67ba88f02efd2b29b02e228a347c6e9ab3dcb7
Michaellis <michael94ellis@gmail.com>
2022-01-04 13:43:48 -0500

    functional bottom bar "texting" event entry area Scrollable content view of the demo and the text entry locks to the bottom

--- Shared/Dashboard.swift
+++ Shared/Dashboard.swift
@@ -1,69 +1,66 @@
+////
+////  Dashboard.swift
+////  TimeDraw
+////
+////  Created by Michael Ellis on 1/3/22.
+////
 //
-//  Dashboard.swift
-//  TimeDraw
+//import SwiftUI
+//import CoreData
 //
-//  Created by Michael Ellis on 1/3/22.
+//struct DashboardView: View {
+//    
+//    @State var newItem: String = ""
 //
-
-import SwiftUI
-import CoreData
-
-struct DashboardView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Goal.start, ascending: true)],
-        animation: .default)
-    private var goals: FetchedResults<Goal>
-    
-    @State var newItem: String = ""
-
-    var body: some View {
-        HStack {
-            Button(action: addItem) {
-                Label("", systemImage: "plus")
-            }
-            TextField("New Item", text: self.$newItem)
-        }
-        VStack {
-            ForEach(self.goals) { item in
-                Text(item.title ?? "No Title")
-                Text(item.start!, formatter: itemFormatter)
-            }
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Goal(context: self.viewContext)
-            newItem.start = Date()
-            newItem.title = self.newItem
-            do {
-                try self.viewContext.save()
-            } catch {
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(_ indicies: IndexSet) {
-        withAnimation {
-            indicies.map { self.goals[$0] }.forEach(self.viewContext.delete)
-
-            do {
-                try self.viewContext.save()
-            } catch {
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-}
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+//    var body: some View {
+//        VStack {
+//            VStack {
+//                ForEach(self.goals) { item in
+//                    Text(item.title ?? "No Title")
+//                    Text(item.start!, formatter: itemFormatter)
+//                }
+//            }
+//            Spacer()
+//            HStack {
+//                Button(action: addItem) {
+//                    Label("", systemImage: "plus")
+//                }
+//                TextField("Add an event", text: self.$newItem)
+//            }
+//        }
+//    }
+//
+//    private func addItem() {
+//        withAnimation {
+//            let newItem = Goal(context: self.viewContext)
+//            newItem.start = Date()
+//            newItem.title = self.newItem
+//            do {
+//                try self.viewContext.save()
+//            } catch {
+//                let nsError = error as NSError
+//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//            }
+//        }
+//    }
+//
+//    private func deleteItems(_ indicies: IndexSet) {
+//        withAnimation {
+//            indicies.map { self.goals[$0] }.forEach(self.viewContext.delete)
+//
+//            do {
+//                try self.viewContext.save()
+//            } catch {
+//                let nsError = error as NSError
+//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//            }
+//        }
+//    }
+//}
+//
+//private let itemFormatter: DateFormatter = {
+//    let formatter = DateFormatter()
+//    formatter.dateStyle = .short
+//    formatter.timeStyle = .medium
+//    return formatter
+//}()
--- Shared/MainView.swift
+++ Shared/MainView.swift
@@ -11,6 +11,12 @@ import CoreData
 struct MainView: View {
     
     @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Goal.start, ascending: true)],
+        animation: .default)
+    private var goals: FetchedResults<Goal>
+    
     private let date = Date()
     private let weekdayFormatter = DateFormatter()
     private let monthNameFormatter = DateFormatter()
@@ -21,115 +27,155 @@ struct MainView: View {
     @State private var dailyGoal: String = "What is your goal today?"
     @FocusState private var isDailyGoalFocused: Bool
     
+    @State var newEventName: String = ""
+    @FocusState private var isNewEventFocused: Bool
+    
     init() {
         self.weekdayFormatter.dateFormat = "EEE"
         self.monthNameFormatter.dateFormat = "LLLL"
     }
     
     var body: some View {
-        ScrollView(.vertical) {
-            HStack {
-                Text(self.monthNameFormatter.string(from: self.date))
-                    .fontWeight(.semibold)
-                    .foregroundColor(Color.red1)
-                Spacer()
-                Button(action: {
-                    self.showMenuPopover.toggle()
-                }, label: {
-                    Image(systemName: "ellipsis")
-                        .frame(width: 40, height: 30)
-                })
-                    .popover(isPresented: self.$showMenuPopover, content: {
-                        PopoverLink(destination: EditGoalView(), label: { Text("New Goal") })
-                        PopoverLink(destination: Text("To Do"), label: { Text("New Event") })
-                    })
-            }
-            .padding(.horizontal, 25)
-            .padding(.vertical, 10)
-            HStack {
-                Spacer()
-                ForEach(Calendar.current.daysWithSameWeekOfYear(as: date), id: \.self) { date in
-                    if Calendar.current.isDateInToday(date) {
-                        VStack {
-                            Text(self.weekdayFormatter.string(from: date))
-                            Spacer()
-                            Text(date.get(.day).formatted())
-                                .fontWeight(.semibold)
-                                .foregroundColor(Color.red1)
-                        }
-                        .frame(maxWidth: .infinity)
-                        .padding(.vertical, 10)
-                        .background(RoundedRectangle(cornerRadius: 12).fill(Color.gray.opacity(0.2)))
-                    } else {
-                        VStack {
-                            Text(self.weekdayFormatter.string(from: date))
-                                .foregroundColor(Color.gray2)
-                            Spacer()
-                            Text(date.get(.day).formatted())
-                        }
-                        .frame(maxWidth: .infinity)
-                        .padding(.vertical, 10)
+        VStack {
+            ScrollView(.vertical) {
+                VStack(spacing: 0) {
+                    HStack {
+                        Text(self.monthNameFormatter.string(from: self.date))
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.red1)
+                        Spacer()
+                        Button(action: {
+                            self.showMenuPopover.toggle()
+                        }, label: {
+                            Image(systemName: "ellipsis")
+                                .frame(width: 40, height: 30)
+                        })
+                            .popover(isPresented: self.$showMenuPopover, content: {
+                                PopoverLink(destination: EditGoalView(), label: { Text("New Goal") })
+                                PopoverLink(destination: Text("To Do"), label: { Text("New Event") })
+                            })
                     }
-                    Spacer()
+                    .padding(.horizontal, 25)
+                    .padding(.vertical, 10)
+                    HStack {
+                        Spacer()
+                        ForEach(Calendar.current.daysWithSameWeekOfYear(as: date), id: \.self) { date in
+                            if Calendar.current.isDateInToday(date) {
+                                VStack {
+                                    Text(self.weekdayFormatter.string(from: date))
+                                        .padding(.bottom, 5)
+                                    Text(date.get(.day).formatted())
+                                        .fontWeight(.semibold)
+                                        .foregroundColor(Color.red1)
+                                }
+                                .frame(maxWidth: .infinity)
+                                .padding(.vertical, 10)
+                                .background(RoundedRectangle(cornerRadius: 12).fill(Color.gray.opacity(0.2)))
+                            } else {
+                                VStack {
+                                    Text(self.weekdayFormatter.string(from: date))
+                                        .foregroundColor(Color.gray2)
+                                        .padding(.bottom, 5)
+                                    Text(date.get(.day).formatted())
+                                }
+                                .frame(maxWidth: .infinity)
+                                .padding(.vertical, 10)
+                            }
+                            Spacer()
+                        }
+                    }
                 }
-            }
-            Spacer()
-            // Goal ?? TODO make set daily goal button
-            if self.dailyGoal == self.emptyGoalText {
-                Button(action: {
-                    self.dailyGoal = ""
-                    self.isDailyGoalFocused = true
-                }, label: {
-                    Text(self.emptyGoalText)
+                // Goal ?? TODO make set daily goal button
+                if self.dailyGoal == self.emptyGoalText {
+                    Button(action: {
+                        self.dailyGoal = ""
+                        self.isDailyGoalFocused = true
+                    }, label: {
+                        Text(self.emptyGoalText)
+                            .foregroundColor(Color.gray1)
+                            .padding(4) // +1 for no border
+                            .padding(.horizontal, 30)
+                            .padding(.vertical, 10)
+                    })
+                        .contentShape(Rectangle())
+                } else {
+                    TextField("", text: self.$dailyGoal)
                         .foregroundColor(Color.gray1)
-                        .padding(4) // +1 for no border
+                        .lineLimit(3)
+                        .multilineTextAlignment(.center)
+                        .submitLabel(.done)
+                        .focused(self.$isDailyGoalFocused)
+                        .onSubmit {
+                            if self.dailyGoal.isEmpty { self.dailyGoal = self.emptyGoalText }
+                        }
+                        .padding(3) // border increases padding by 1
+                        .background(RoundedRectangle(cornerRadius: 4).stroke(self.isDailyGoalFocused ? Color.gray : Color.clear))
                         .padding(.horizontal, 30)
                         .padding(.vertical, 10)
+                }
+                Spacer()
+                // Clock View
+                if !self.hideClockView {
+                    Image(systemName: "clock")
+                        .resizable()
+                        .frame(width: 130, height: 130)
+                        .padding(25)
+                }
+                Button(action: {
+                    self.hideClockView.toggle()
+                }, label: {
+                    Group {
+                        if self.hideClockView {
+                            Image(systemName: "chevron.down")
+                                .resizable()
+                                .frame(width: 20, height: 10)
+                        } else {
+                            Image(systemName: "chevron.up")
+                                .resizable()
+                                .frame(width: 20, height: 10)
+                        }
+                    }
+                    .frame(width: 200, height: 30)
+                    .frame(maxWidth: .infinity)
+                    .background(RoundedRectangle(cornerRadius: 12).fill(Color.gray.opacity(0.05)))
                 })
                     .contentShape(Rectangle())
-            } else {
-                TextField("", text: self.$dailyGoal)
-                    .foregroundColor(Color.gray1)
-                    .lineLimit(3)
-                    .multilineTextAlignment(.center)
-                    .submitLabel(.done)
-                    .focused(self.$isDailyGoalFocused)
-                    .onSubmit {
-                        if self.dailyGoal.isEmpty { self.dailyGoal = self.emptyGoalText }
-                    }
-                    .padding(3) // border increases padding by 1
-                    .background(RoundedRectangle(cornerRadius: 4).stroke(self.isDailyGoalFocused ? Color.gray : Color.clear))
-                    .padding(.horizontal, 30)
-                    .padding(.vertical, 10)
-            }
-            Spacer()
-            // Clock View
-            if !self.hideClockView {
-                Image(systemName: "clock")
-                    .resizable()
-                    .frame(width: 130, height: 130)
-                    .padding(25)
-            }
-            Button(action: {
-                self.hideClockView.toggle()
-            }, label: {
-                Group {
-                    if self.hideClockView {
-                        Image(systemName: "chevron.down")
-                            .resizable()
-                            .frame(width: 20, height: 10)
-                    } else {
-                        Image(systemName: "chevron.up")
-                            .resizable()
-                            .frame(width: 20, height: 10)
+                VStack {
+                    ForEach(self.goals) { item in
+                        HStack {
+                            Text(item.title ?? "No Title")
+                            Spacer()
+                            Text(item.start?.formatted() ?? "??")
+                        }
+                        .padding()
                     }
                 }
-                .frame(width: 200, height: 30)
-                .frame(maxWidth: .infinity)
-                .background(RoundedRectangle(cornerRadius: 12).fill(Color.gray.opacity(0.05)))
-            })
-                .contentShape(Rectangle())
-            DashboardView()
+                Spacer()
+            }
+            VStack(spacing: 0) {
+                Group {
+                    Divider().padding(.horizontal)
+                    HStack {
+                        Button(action: addItem) {
+                            Image(systemName: "plus")
+                        }.padding(.horizontal)
+                            .buttonStyle(.plain)
+                        TextField("Add an event", text: self.$newEventName)
+                            .focused(self.$isNewEventFocused)
+                    }
+                }
+                .padding()
+                .background(Color(uiColor: .systemBackground))
+            }
+        }
+    }
+    
+    private func addItem() {
+        withAnimation {
+            let newItem = Goal(context: self.viewContext)
+            newItem.start = Date()
+            newItem.title = self.newEventName
+            CoreDataManager.shared.saveMainContext()
         }
     }
 }

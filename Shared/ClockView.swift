57ba835a3ffe52f76e1d68d11bbf7dc7679bcd23
Michaellis <michael94ellis@gmail.com>
2022-03-09 22:24:11 -0500

    Basic clock event drawing class made

--- Shared/ClockView.swift
+++ Shared/ClockView.swift
@@ -7,13 +7,14 @@
 
 import SwiftUI
 
+struct Time {
+    var sec: Int
+    var min: Int
+    var hour: Int
+}
+
 struct TimeDrawClock: View {
     
-    struct Time {
-        var sec: Int
-        var min: Int
-        var hour: Int
-    }
     
     @State var currentTime = Time(sec: 0, min: 0, hour: 0)
     @State var timer = Timer.publish(every: 1, on: .current, in: .default).autoconnect()
@@ -78,7 +79,11 @@ struct TimeDrawClock: View {
                 self.clockHands
                 ForEach(EventListViewModel.shared.events ,id: \.self) { event in
                     PartialCircleBorder(start: event.startDate, end: event.endDate, radius: 180)
-                    .foregroundColor(.red2)
+                        .foregroundColor(Color(cgColor: event.calendar.cgColor))
+                }
+                ForEach(EventListViewModel.shared.reminders ,id: \.self) { reminder in
+                    PartialCircleBorder(startComponents: reminder.startDateComponents, endComponents: reminder.dueDateComponents, radius: 180)
+                        .foregroundColor(Color(reminder.calendar.cgColor))
                 }
             }
             .frame(width: width - 80, height: width - 80)
@@ -101,16 +106,72 @@ struct TimeDrawClock: View {
     }
 }
 
-struct PartialCircleBorder : Shape {
+struct PartialCircleBorder: Shape {
     
-    let start: Date
-    let end: Date
+    // Minute is 0.5 Degrees
+    // Hour is 30 Degrees
+    let start: Date?
+    let end: Date?
+    let startComponents: DateComponents?
+    let endComponents: DateComponents?
     let radius: Double
+    var startDegrees: Double = 0.0
+    var endDegrees: Double = 0.0
+    
+    func getAngle(for date: Date? = nil, with components: DateComponents? = nil) -> Double {
+        if let date = date {
+            return Double(date.get(.hour) * 30) + Double(date.get(.minute)) * 0.5
+        } else if let hour = components?.hour,
+                  let min = components?.minute {
+            return Double(hour) * 30.0 + Double(min) * 0.5
+        } else {
+            return 0.0
+        }
+    }
+    
+    init(start: Date, end: Date, radius: Double) {
+        self.start = start
+        self.end = end
+        self.startComponents = nil
+        self.endComponents = nil
+        self.radius = radius
+        self.startDegrees = self.getAngle(for: start)
+        self.endDegrees = self.getAngle(for: end)
+    }
+    init(startComponents: DateComponents?, endComponents: DateComponents?, radius: Double) {
+        self.startComponents = startComponents ?? DateComponents()
+        self.endComponents = endComponents ?? DateComponents()
+        self.start = nil
+        self.end = nil
+        self.radius = radius
+        self.startDegrees = self.getAngle(with: startComponents)
+        self.endDegrees = self.getAngle(with: endComponents)
+    }
     
     func path(in rect: CGRect) -> Path {
         var p = Path()
-        p.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: self.radius, startAngle: .degrees(Double(self.start.get(.hour) * 30)), endAngle: .degrees(Double(self.end.get(.hour) * 30)), clockwise: false)
+        p.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: self.radius, startAngle: .degrees(self.startDegrees), endAngle: .degrees(self.endDegrees), clockwise: false)
 
         return p.strokedPath(.init(lineWidth: 16))
+    }
+}
+
+
+struct PartialCircleBorder2: Shape {
+    
+    // Minute is 0.5 Degrees
+    // Hour is 30 Degrees
+    let start: Time
+    let end: Time
+    let radius: Double
+    
+    func getAngle(for time: Time) -> Double {
+        return Double(time.hour) * 30.0 + Double(time.min) * 0.5
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: self.radius, startAngle: .degrees(self.getAngle(for: start)), endAngle: .degrees(self.getAngle(for: end)), clockwise: false)
+        return path.strokedPath(.init(lineWidth: 16))
     }
 }

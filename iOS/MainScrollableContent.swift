775f3d510bba9ebcbc6e315c6842aadc3495b885
Michaellis <michael94ellis@gmail.com>
2023-09-15 22:28:57 -0400

    Fix list a bit

--- iOS/MainScrollableContent.swift
+++ iOS/MainScrollableContent.swift
@@ -12,32 +12,36 @@ struct MainScrollableContent: View {
     
     @EnvironmentObject var itemList: CalendarItemListViewModel
     @ObservedObject private var appSettings: AppSettings = .shared
+    let clockHorizPadding: CGFloat = 100
+    let clockVertPadding: CGFloat = 20
     
     var itemCount: Int { itemList.events.count + itemList.reminders.count }
     
     var body: some View {
-        List {
-            if self.appSettings.isTimeDrawClockEnabled {
-                HStack {
-                    Spacer()
-                    GeometryReader { geo in
-                        TimeDrawClock(width: geo.size.width)
-                    }
-                    Spacer()
+        GeometryReader { geo in
+            List {
+                if self.appSettings.isTimeDrawClockEnabled {
+                    TimeDrawClock(width: geo.size.width - clockHorizPadding)
+                        .listRowSeparator(.hidden)
+                        .padding(.vertical, clockVertPadding)
+                        .padding(.horizontal, clockHorizPadding)
                 }
+                ForEach(self.itemList.events) { item in
+                    EventListCell(item: item)
+                        .listRowSeparator(.hidden)
+                }
+                ForEach(self.itemList.reminders) { item in
+                    ReminderListCell(item: item)
+                        .listRowSeparator(.hidden)
+                }
+                Spacer(minLength: itemCount > 0 ? 60 : 0)
+                    .listRowSeparator(.hidden)
             }
-            ForEach(self.itemList.events) { item in
-                EventListCell(item: item)
-            }
-            ForEach(self.itemList.reminders) { item in
-                ReminderListCell(item: item)
-            }
-            Spacer(minLength: itemCount > 0 ? 60 : 0)
+            .buttonStyle(.plain)
+            .listStyle(.plain)
+            .refreshable(action: {
+                self.itemList.updateData()
+            })
         }
-        .buttonStyle(.plain)
-        .listStyle(.plain)
-        .refreshable(action: {
-            self.itemList.updateData()
-        })
     }
 }
